f22b52a7ee8037a28d33706c0b0dfeeab3a34447
Johnny <141275365+snrqz@users.noreply.github.com>
2024-08-24 18:49:01 +0300

    colors.h

---
+++ colors/colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+void display_red(void);
+void display_green(void);
+void display_blue(void);
+void display_cyan(void);
+void display_magenta(void);
+void display_yellow(void);
+void display_white(void);
+void display_reset(void);
+
+#endif
