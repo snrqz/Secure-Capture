eb42fd5ba7b4844dc654fd6d79fa5afc7892b599
Johnny <141275365+snrqz@users.noreply.github.com>
2024-08-24 18:48:09 +0300

    screen.h

---
+++ Screen/screen.h
@@ -0,0 +1,15 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+size_t capture_screen(char* buffer, size_t buffer_size);
+const char** printS();
+const char** printH();
+const char** printU();
+const char** printT();
+const char** printD();
+const char** printO();
+const char** printW();
+const char** printN();
+void printSHUTDOWN();
+
+#endif
